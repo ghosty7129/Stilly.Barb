c380026b7b6666d6d90137db8419a3f4f3989cb7
Barber Shop Admin <barber@unusualbarbershop.com>
2026-05-02 14:48:05 +0300

    fix: scroll to top when navigating to privacy policy page

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const PrivacyPolicy = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <div className="min-h-screen bg-neutral-50 pt-28 sm:pt-24">
       <Header />
